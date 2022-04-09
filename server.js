f761bd6ef557dbe8a0df58005c8a09ce73e60e34
Jery Bopara <jerrybopara@gmail.com>
2022-04-10 00:41:27 +0530

    Main - Changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World. It is my - QA branch. Build-1');
+  res.send('Hello World. It is my - QA branch. Build-2');
 });
 
 app.listen(PORT, HOST);
