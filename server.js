1b4832a243360220c60a2d22608c258257acc74b
Jerry Bopara <jerrybopara@gmail.com>
2022-04-09 11:32:32 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World. With code which delete older image..');
+  res.send('Hello World. Running From AWS ECS 1');
 });
 
 app.listen(PORT, HOST);
